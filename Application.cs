a6b7c7e92c9fbfd3f6683deffa76f0ef0dea2818
ChiAkuma <fireflo713@gmail.com>
2023-10-09 22:29:27 +0200

    Console prettifying and refactor

--- Application.cs
+++ Application.cs
@@ -15,12 +15,20 @@ namespace ServerCloud
         internal static string workingDir = Directory.GetCurrentDirectory();
         internal static Application cloud = new Application();
         internal static YAML yaml = new YAML();
+        internal static bool isRunning = true;
+        private DBConnector db;
 
         static void Main(string[] args)
         {
+            isRunning = true;
             AnsiConsole.Clear();
             AnsiConsole.Record();
-            AnsiConsole.MarkupLine("[underline #00ff00]Hello[/] World!");
+            AnsiConsole.WriteLine("");
+            AnsiConsole.Write(
+                new FigletText("ServerCloud")
+                    .Centered()
+                    .Color(Color.Red));
+            AnsiConsole.WriteLine("");
 
             if (args.Length == 0)
             {
@@ -28,12 +36,12 @@ namespace ServerCloud
             }
             else
             {
-                AnsiConsole.MarkupLine($"Initializing instance in Directory: [#ff3333]{args[0]}[/]");
+                AnsiConsole.MarkupLine($"Initializiere Datenordner: [#ff3333]{args[0]}[/]");
                 workingDir = args[0];
             }
             //Here begins the logic
 
-            DBConnector db = new DBConnector();
+            cloud.db = new DBConnector();
             /* 
              * Modules:
              * Database: PluginSync
@@ -46,20 +54,55 @@ namespace ServerCloud
              */
 
             //Program is being closed
-            var table = new Table().Centered();
-            
-            AnsiConsole.MarkupLine("[#ff00ff]Taste drücken um zu beenden...[/]");
+            cloud.ModuleSelector();
+        }
 
-            Console.ReadKey(true);
-            
-            AnsiConsole.WriteLine("Programm beendet!");
+        public void ModuleSelector()
+        {
+            string choice = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("\n[#eecc33]Was möchtest du ausführen?[/]")
+                    .PageSize(10)
+                    .MoreChoicesText("[grey](Gehe hoch und runter um mehr optionen zu finden)[/]")
+                    .AddChoices(new[] {
+                        "1. Datenordner anpassen", "2. SQL Daten überprüfen" , "3. Programm Beenden",
+                    }));
+
+            Rule rule = new Rule();
+            rule.Style = Style.Parse("green dim");
+            AnsiConsole.Write(rule);
+            AnsiConsole.MarkupLine($"[#00ff00]{choice}[/]");
+            AnsiConsole.Write(rule);
+            switch (int.Parse(choice.Split(".")[0]))
+            {
+                case 1:
+                    cloud.workingDirectoryQuestion(true);
+                    cloud.ModuleSelector();
+                    break;
+                case 2:
+                    cloud.db.SQLInformationCheck();
+                    cloud.ModuleSelector();
+                    break;
+                case 3:
+                default:
+                    cloud.shutdownProgram();
+                    break;
+            }
+        }
+
+        public void shutdownProgram()
+        {
+            AnsiConsole.WriteLine("Programm wird beendet!");
+
             FileStream fs = File.Create("jungle.htm");
             StreamWriter sw = new StreamWriter(fs);
             sw.Write(AnsiConsole.ExportHtml());
             sw.Flush();
             sw.Close();
             fs.Close();
-            System.Environment.Exit(0);
+
+            isRunning = false;
+            //System.Environment.Exit(0);
         }
 
         public void workingDirectoryQuestion(bool overrideSkip = false)
@@ -72,6 +115,8 @@ namespace ServerCloud
             {
                 AnsiConsole.MarkupLine($"Um den Pfad zu ändern tippe jetzt den gewünschten Pfad ein:");
                 workingDir = AnsiConsole.Prompt(new TextPrompt<string>("Datenordner").DefaultValue<string>(workingDir));
+                //TODO: Daten in neuen Ordner schieben?
+                AnsiConsole.MarkupLine($"Initializiere neuen Datenordner: [#ff3333]{workingDir}[/]");
             };
             config.workingDir = workingDir;
             config.firstStart = false;
--- Database/DBConnector.cs
+++ Database/DBConnector.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,8 +14,13 @@ namespace ServerCloud.Database
 {
     internal class DBConnector
     {
+        //The connection to the database
+        MySqlConnection connection = new MySqlConnection();
+
+        //Basic sql connector and creating of basic tables if not exists
         public DBConnector()
         {
+            new Thread(ConnectionTest).Start();
             ConfigFile config = Application.yaml.load();
             if (config.sqlServer == "" || config.sqlUser == "" || config.sqlPass == "" || config.sqlDb == "")
             {
@@ -22,16 +28,15 @@ namespace ServerCloud.Database
             }
 
             bool isClosed = true;
-            MySqlConnection connection = new MySqlConnection();
+            
             while (isClosed)
             {
                 try
                 {
-                    connection.ConnectionString = $"Server={config.sqlServer};User ID={config.sqlUser};Password={config.sqlPass};Database={config.sqlDb}";
-                    connection.StateChange += Connection_StateChange;
-                    connection.OpenAsync();
+                    this.connection.ConnectionString = $"Server={config.sqlServer};User ID={config.sqlUser};Password={config.sqlPass};Database={config.sqlDb}";
+                    this.connection.StateChange += Connection_StateChange;
+                    this.connection.Open();
                     isClosed = false;
-                    
                 }
                 catch (MySqlException e)
                 {
@@ -40,16 +45,20 @@ namespace ServerCloud.Database
                     isClosed = true;
                 }
             }
-            MySqlCommand cmd_pinfo = new MySqlCommand("CREATE TABLE IF NOT EXISTS PluginInfo (name TEXT, downloadLink TEXT, currentVersion TEXT, updateAvailable BOOL, syncActive BOOL, configSync BOOL, configUsePlaceholders BOOL);", connection);
-            cmd_pinfo.ExecuteNonQueryAsync();
 
+            //Create Tables for my Datatypes
+            //Table for all plugin information /saved in choosen directory
+            MySqlCommand cmd_pinfo = new MySqlCommand("CREATE TABLE IF NOT EXISTS PluginInfo (name TEXT, downloadLink TEXT, currentVersion TEXT, updateAvailable BOOL, syncActive BOOL, configSync BOOL, configUsePlaceholders BOOL);", connection);
+            cmd_pinfo.ExecuteNonQuery();
+
+            //If the plugin will be synced and to what server
             MySqlCommand cmd_psync = new MySqlCommand("CREATE TABLE IF NOT EXISTS PluginSync (name TEXT, server TEXT);", connection);
-            cmd_psync.ExecuteNonQueryAsync();
+            cmd_psync.ExecuteNonQuery();
         }
 
         private void Connection_StateChange(object sender, StateChangeEventArgs e)
         {
-            AnsiConsole.WriteLine($"Sql Verbindung: {e.CurrentState}");
+            AnsiConsole.WriteLine($"Sql Status: {e.CurrentState}");
         }
 
         public ConfigFile SQLInformation(ConfigFile config)
@@ -61,6 +70,38 @@ namespace ServerCloud.Database
             config.sqlDb = AnsiConsole.Prompt(new TextPrompt<string>("SQL Datenbank").DefaultValue<string>(config.sqlDb));
             Application.yaml.save(config);
             return config;
+        }
+
+        public void SQLInformationCheck()
+        {
+            ConfigFile config = Application.yaml.load();
+            AnsiConsole.WriteLine($"Hier sind deine SQL Daten: (natürlich ohne Passwort!)\n");
+            AnsiConsole.WriteLine($"SQL Server IP:      {config.sqlServer}");
+            AnsiConsole.WriteLine($"SQL Benutzer:       {config.sqlUser}");
+            AnsiConsole.WriteLine($"SQL Datenbank:      {config.sqlDb}");
+        }
+
+        //Ja kp ob ich das brauche.
+        //Reopens connection every 2 seconds if closed
+        //Can be changed to a execute function that opens the connection before the execution
+        public void ConnectionTest()
+        {
+            while (Application.isRunning)
+            {
+                Thread.Sleep(2000);
+                //AnsiConsole.WriteLine("Thread ConnectionTest is running!");
+                this.connection.Ping();
+                if (this.connection.State == ConnectionState.Closed)
+                {
+                    try
+                    {
+                        this.connection.Open();
+                    }
+                    catch (MySqlException) { }
+                }
+                
+            }
+            AnsiConsole.WriteLine("SQL Hintergrund Thread beendet.");
         }
     }
 }
